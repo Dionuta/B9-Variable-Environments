d0e12a05c4370b76bc2274276f645024c22ed2ad
Dionuta <diounta@gmail.com>
2019-02-03 20:24:36 -0800

    notes

--- B9_VariableEnvironments/Starter/app.js
+++ B9_VariableEnvironments/Starter/app.js
@@ -1,4 +1,17 @@
 function b() {
-    var myVar;
-    console.log(myVar);
+    var myVar; // it's in the function 'b' execution context
+    console.log(myVar);//happens 3rd log = undefined
 }
+
+function a(){
+    var myVar = 2; //  it's in the function 'a' execution context
+    console.log(myVar); //happens 2nd log = 2
+    b();
+}
+
+var myVar = 1;// Global execution context 
+console.log(myVar);//happens 1st log = 1
+a();
+console.log(myVar)// happens 4th log = 1
+
+//all myVar are not related due to execution context. 
